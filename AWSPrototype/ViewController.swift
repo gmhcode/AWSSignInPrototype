59747be2e2437b4e85d50da88f6bcb5fbce33b76
Greg Hughes <dcgg_101@hotmail.com>
2020-03-26 19:13:33 -0600

    initial

---
+++ AWSPrototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AWSPrototype
+//
+//  Created by Greg Hughes on 3/26/20.
+//  Copyright © 2020 Greg Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
